c9ba334fc590a5f6e7a3b8c013d89bae66c339ee
wasja1982 <wasja@yandex.ru>
2014-05-25 16:06:25 +0300

    Исправлена неточность с сортировкой по числу просмотров.

--- classes/modules/topic/mapper/Topic.mapper.class.php
+++ classes/modules/topic/mapper/Topic.mapper.class.php
@@ -20,16 +20,34 @@ class PluginViews_ModuleTopic_MapperTopic extends PluginViews_Inherit_ModuleTopi
      * @return array
      */
     public function GetTopics($aFilter,&$iCount,$iCurrPage,$iPerPage) {
-        if(!isset($aFilter['viewstat']) || !$aFilter['viewstat']) {
+        if(!isset($aFilter['viewstat'])) {
             return parent::GetTopics($aFilter,$iCount,$iCurrPage,$iPerPage);
         } else {
             $sWhere=$this->buildFilter($aFilter);
-
-            if (!Config::Get('plugin.views.stat_date_filter')) {
+            if (!$aFilter['viewstat']) {
                 $sql = "SELECT
-                                t.topic_id, count(s.stat_id)+t.topic_count_read as count
+                                t.topic_id, v.topic_count_read as count
                             FROM
                                 ".Config::Get('db.table.blog')." as b,
+                                ".Config::Get('db.table.topic_view')." as v,
+                                ".Config::Get('db.table.topic')." as t
+                            WHERE
+                                1=1
+                                ".$sWhere."
+                                AND
+                                t.blog_id=b.blog_id
+                                AND
+                                t.topic_id=v.topic_id
+                            GROUP BY t.topic_id
+                            ORDER BY count DESC, t.topic_id DESC
+                            LIMIT ?d, ?d";
+            } else {
+                if (!Config::Get('plugin.views.stat_date_filter')) {
+                    $sql = "SELECT
+                                t.topic_id, count(s.stat_id)+v.topic_count_read as count
+                            FROM
+                                ".Config::Get('db.table.blog')." as b,
+                                ".Config::Get('db.table.topic_view')." as v,
                                 ".Config::Get('db.table.topic')." as t
                             LEFT JOIN vc_stat as s
                                 ON t.topic_id=s.stat_topic_id
@@ -38,14 +56,17 @@ class PluginViews_ModuleTopic_MapperTopic extends PluginViews_Inherit_ModuleTopi
                                 ".$sWhere."
                                 AND
                                 t.blog_id=b.blog_id
+                                AND
+                                t.topic_id=v.topic_id
                             GROUP BY t.topic_id
                             ORDER BY count DESC, t.topic_id DESC
                             LIMIT ?d, ?d";
-            } else {
-                $sql = "SELECT
+                } else {
+                    $sql = "SELECT
                                 t.topic_id, count(s.stat_id)+t.topic_count_read as count
                             FROM
                                 ".Config::Get('db.table.topic')." as t,
+                                ".Config::Get('db.table.topic_view')." as v,
                                 ".Config::Get('db.table.blog')." as b,
                                 vc_stat as s
                             WHERE
@@ -55,10 +76,13 @@ class PluginViews_ModuleTopic_MapperTopic extends PluginViews_Inherit_ModuleTopi
                                 t.blog_id=b.blog_id
                                 AND
                                 t.topic_id=s.stat_topic_id
+                                AND
+                                t.topic_id=v.topic_id
                                 " . (isset($aFilter['stat_date_more']) ? " AND s.stat_date > '".mysql_real_escape_string($aFilter['stat_date_more'])."'" : "") . "
                             GROUP BY s.stat_topic_id
                             ORDER BY count DESC
                             LIMIT ?d, ?d";
+                }
             }
             $aTopics=array();
             if ($aRows=$this->oDb->selectPage($iCount,$sql,($iCurrPage-1)*$iPerPage, $iPerPage)) {
@@ -69,24 +93,24 @@ class PluginViews_ModuleTopic_MapperTopic extends PluginViews_Inherit_ModuleTopi
             return $aTopics;
         }
     }
-    
-	public function AddView($sTopicId) {
-        $sql = "UPDATE " . Config::Get('db.table.topic_view') . "  SET `topic_count_read`=`topic_count_read`+1 WHERE `topic_id`=?d";
-		if ($this->oDb->query($sql, $sTopicId)) {
-			return true;
-		}
-		return false;
-	}
 
-	public function GetCountRead($sTopicId) {
-		$sql = "SELECT topic_count_read FROM " . Config::Get('db.table.topic_view') . " WHERE topic_id = ?d";
-		if ($aRow = $this->oDb->selectRow($sql, $sTopicId)) {
-			return $aRow['topic_count_read'];
-		} else {
+    public function AddView($sTopicId) {
+        $sql = "UPDATE " . Config::Get('db.table.topic_view') . "  SET `topic_count_read`=`topic_count_read`+1 WHERE `topic_id`=?d";
+        if ($this->oDb->query($sql, $sTopicId)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function GetCountRead($sTopicId) {
+        $sql = "SELECT topic_count_read FROM " . Config::Get('db.table.topic_view') . " WHERE topic_id = ?d";
+        if ($aRow = $this->oDb->selectRow($sql, $sTopicId)) {
+            return $aRow['topic_count_read'];
+        } else {
             $sql = "INSERT INTO " . Config::Get('db.table.topic_view') . " (`topic_id`, `topic_count_read`) VALUES (?d, 0)";
             $this->oDb->query($sql, $sTopicId);
             return 0;
         }
-	}
+    }
 }
 ?>
